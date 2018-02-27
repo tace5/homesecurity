6d44e547c1741033c1ba96efabd6e286a29c15c6
Linus Bein Fahlander <linus.webdevelopment@gmail.com>
2018-02-27 19:13:48 +0100

    Created files for buzzer

---
+++ src/drivers/buzzer/buzzer.h
@@ -0,0 +1,12 @@
+//
+// Created by Linus Bein Fahlander on 2018-02-27.
+//
+
+#ifndef HOMESECURITY_BUZZER_H
+#define HOMESECURITY_BUZZER_H
+
+//void buzz(int duration); TODO
+
+//#include "buzzer.c"
+
+#endif //HOMESECURITY_BUZZER_H
