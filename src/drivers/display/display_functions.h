8c279d188d01e5de6711be82eed832e95d0b8c07
LinusBF <linus.webdevelopment@gmail.com>
2018-02-27 11:24:07 +0100

    Created files for using the on-board display and implemented the basic functions from labs

---
+++ src/drivers/display/display_functions.h
@@ -0,0 +1,41 @@
+/* display_functions.h
+   Header file for all labs.
+   This file written 2015 by F Lundevall
+   Some parts are original code written by Axel Isaksson
+
+   Latest update 2015-08-28 by F Lundevall
+
+   For copyright and licensing, see file COPYING */
+
+#ifndef HOMESECURITY_DISPLAY_FUNCTIONS_H
+#define HOMESECURITY_DISPLAY_FUNCTIONS_H
+
+/* Declare display-related functions from mipslabfunc.c */
+void display_init(void);
+void display_string(int line, char *s);
+void display_update(void);
+uint8_t spi_send_recv(uint8_t data);
+
+/* Declare lab-related functions from mipslabfunc.c */
+char * itoaconv( int num );
+void quicksleep(int cyc);
+
+/* Declare display_debug - a function to help debugging.
+
+   After calling display_debug,
+   the two middle lines of the display show
+   an address and its current contents.
+
+   There's one parameter: the address to read and display.
+
+   Note: When you use this function, you should comment out any
+   repeated calls to display_image; display_image overwrites
+   about half of the digits shown by display_debug.
+*/
+void display_debug( volatile int * const addr );
+
+void display_setup(void);
+
+#include "display_functions.c"
+
+#endif //HOMESECURITY_DISPLAY_FUNCTIONS_H
---
+++ src/drivers/display/displaysetup.c
@@ -0,0 +1,13 @@
+/* displaysetup.c
+
+   This file written 2015 by Axel Isaksson,
+   modified 2015, 2017 by F Lundevall
+
+   Latest update 2017-04-21 by F Lundevall
+
+   For copyright and licensing, see file COPYING */
+
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>  /* Declarations of system-specific addresses etc */
+#include "display_functions.h"
+
