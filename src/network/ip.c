cb4af6acab354d75ed5d26bfb53a4c7abdec46d5
Ruben Hume <hume@kth.se>
2018-03-04 16:10:46 +0100

    Changed ip header function to write to ethernet buffer directly.

--- src/network/ip.c
+++ src/network/ip.c
@@ -1,33 +1,37 @@
 
-void construct_ipv4_header(int * header, uint16_t data_length, uint8_t ttl, uint8_t protocol, int source_ip, int dest_ip) {
-  uint8_t ihl = 0x5;
-  uint16_t total_length = ihl * 4 + data_length;
-  uint8_t ecn = 0x0;
-  uint8_t dscp = 0x0;
-  uint8_t version = 0x4;
+void construct_ipv4_header(uint32_t data_length, uint32_t ttl, uint32_t protocol, uint32_t source_ip, uint32_t dest_ip) {
+  uint32_t header[5];
 
-  uint8_t flag_offset = 0x0;
-  uint8_t flags = 0x2;
-  uint16_t identification = 0x0;
+  uint32_t version = 0x4;
+  uint32_t ihl = 0x5;
+  uint32_t dscp = 0x0;
+  uint32_t ecn = 0x0;
+  uint32_t total_length = ihl * 4 + data_length;
 
-  header[0] = total_length;
-  header[0] |= (ecn << 16);
-  header[0] |= (dscp << 18);
+  uint32_t identification = 0x0;
+  uint32_t flags = 0x2;
+  uint32_t fragment_offset = 0x0;
+
+  header[0] = (version << 28);
   header[0] |= (ihl << 24);
-  header[0] |= (version << 28);
+  header[0] |= (dscp << 18);
+  header[0] |= (ecn << 16);
+  header[0] |= total_length;
 
-  header[1] = flag_offset;
+  header[1] = (identification << 16);
   header[1] |= (flags << 13);
-  header[1] |= (identification << 16);
+  header[1] |= fragment_offset;
 
-  header[2] = (protocol << 16);
-  header[2] |= (ttl << 24);
+  header[2] = (ttl << 24);
+  header[2] |= (protocol << 16);
 
   header[3] = source_ip;
   header[4] = dest_ip;
 
-  uint16_t checksum = calculate_checksum(header);
+  uint32_t checksum = (uint32_t) calculate_checksum(header);
   header[2] |= checksum;
+
+  write_buffer_memory((uint8_t *) header, (ihl * 4));
 }
 
 uint16_t calculate_checksum(int * header) {
