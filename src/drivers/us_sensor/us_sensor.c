6f01d83d7f027913be80a0b49f32a1501b64c41e
Linus Bein Fahlander <linus.webdevelopment@gmail.com>
2018-04-22 19:06:52 +0200

    Added header include and change tab size to 4

--- src/drivers/us_sensor/us_sensor.c
+++ src/drivers/us_sensor/us_sensor.c
@@ -1,35 +1,37 @@
 #include "../../utils/utils.h"
+#include "us_sensor.h"
 
 void sensor_init(void) {
-  TRISECLR = TRIG;
-  TRISESET = ECHO;
+    TRISECLR = TRIG;
+    TRISESET = ECHO;
 
-  T1CON = 0x0;
-  T1CONSET = 0x30;
+    T1CON = 0x0;
+    T1CONSET = 0x30;
 }
 
 void send_pulse(void) {
-  PORTESET = TRIG;
+    PORTESET = TRIG;
 
-  // Hold pulse for 10 us
-  TMR1 = 0x0;
-  T1CONSET = 0x8000; // Start timer
-  while (TMR1 < 800);
-  T1CONCLR = 0x8000;
+    // Hold pulse for 10 us
+    TMR1 = 0x0;
+    T1CONSET = 0x8000; // Start timer
+    while (TMR1 < 800);
+    T1CONCLR = 0x8000;
 
-  PORTECLR = TRIG;
+    PORTECLR = TRIG;
 }
 
+//Returns the distance in cm
 int measure_dist(void) {
-  send_pulse();
-  TMR1 = 0x0; // Reset timer
-  while (!(PORTE & ECHO)); // Wait for Response from sensor
+    send_pulse();
+    TMR1 = 0x0; // Reset timer
+    while (!(PORTE & ECHO)); // Wait for Response from sensor
 
-  T1CONSET = 0x8000; // Start timer
-  while (PORTE & ECHO);  // Measure hold time of response pulse
-  T1CONCLR = 0x8000; // Stop timer
+    T1CONSET = 0x8000; // Start timer
+    while (PORTE & ECHO);  // Measure hold time of response pulse
+    T1CONCLR = 0x8000; // Stop timer
 
-  //display_debug(1, &TMR1);
+    //display_debug(1, &TMR1);
 
-  return ((TMR1 * 256) / (80 * 58));
+    return ((TMR1 * 256) / (80 * 58));
 }
